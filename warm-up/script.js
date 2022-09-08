aebe988348bb611e8650729c81d05dc19d60fe2a
John P <70922864+jpierce-coder@users.noreply.github.com>
2022-09-08 18:38:25 -0500

    Warm Up Exercises

--- warm-up/script.js
+++ warm-up/script.js
@@ -2,3 +2,19 @@ function backgroundToBlue() {
   document.getElementById('body').style.backgroundColor = 'lightblue'
 }
 
+function colorChanged() {
+  console.log('Color Changed');
+  const colorVar = this.value;
+  document.body.style.backgroundColor = colorVar;
+  const p = document.createElement('p');
+  p.innerHTML = 'The form submission';
+  document.appendChild(p);
+}
+
+// const userColor = document.getElementById('backgroundColorInput');
+// userColor.addEventListener('change', colorChanged);
+
+
+
+
+
